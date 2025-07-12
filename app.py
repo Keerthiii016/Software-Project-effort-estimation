97ed71bfbf7578d31eb3ffe1e6a055fc39efc1a0
keerthii016 <keerthiii016@gmail.com>
2025-07-12 15:51:59 +0530

    added port

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import re
 
 
+
+
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
 
@@ -337,6 +339,10 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+  
+
+
 
 
 
